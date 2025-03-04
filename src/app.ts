02b61f4bd989f30578935b88c413b35579a54968
Nur Mohammad Sadek Ali <sadekali1530@gmail.com>
2025-03-05 04:19:01 +0600

    update schema

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,9 @@ import cors from "cors";
 const app: Application = express();
 
 app.use(cors());
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}))
 app.get("/", (req: Request, res: Response) => {
   res.send({
     Message: "this is ph healthCare ....",
